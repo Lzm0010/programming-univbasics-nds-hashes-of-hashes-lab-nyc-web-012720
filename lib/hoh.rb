213bdb5e9890e0740c38d0ba42b03d3702b94556
Lee McCormick <lzm0010@gmail.com>
2020-01-22 17:35:25 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -47,7 +47,9 @@ def naming_system
                       label: "Phylum",
                       sub_category: {
                                       label: "Class",
-                                      sub_category
+                                      sub_category: {
+                                                      label: "Order"
+                                      }
                                     }
                     }
   }
